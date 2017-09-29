0802267d809457d8eecbafb8838fe8f523d02691
erictherobot <erictherobot@outlook.com>
2017-09-29 13:44:02 -0700

    updated webtest001 to install apache

--- site.pp
+++ site.pp
@@ -4,3 +4,12 @@
       content => "Here is my Public IP Address: ${ipaddress_eth1}.\n",  # note the ipaddress_eth0 fact
     }
 
+    node 'webtest001' {
+      class { 'apache': }             # use apache module
+      apache::vhost { 'example.com':  # define vhost resource
+        port    => '80',
+        docroot => '/var/www/html'
+      }
+    }
+
+    # node default {}       # uncomment this line if it doesn't already exist in your manifest
