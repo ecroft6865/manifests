5d23d787cd2008229c833d5bf091d893b8825d5a
erictherobot <erictherobot@outlook.com>
2017-10-10 08:01:53 -0700

    d

--- site.pp
+++ site.pp
@@ -1,18 +1,19 @@
-    file {'/tmp/example-ip':                                            # resource type file and filename
+file {'/tmp/example-ip':                                            # resource type file and filename
       ensure  => present,                                               # make sure it exists
       mode    => '0644',                                                # file permissions
       content => "Here is my Public IP Address: ${ipaddress_eth1}.\n",  # note the ipaddress_eth0 fact
     }
 
-node 'webtest001' {  
-    file { '/tmp/only_on_webtest001':    
-	content => "I'm only needed on the node named 'web1'!",  }
+node 'webtest001' 
+    file { '/tmp/only_on_webtest001':
+        content => "I'm only needed on the node named 'web1'!",  }
 
 
       file { '/home/test':
-	ensure => present,
+        ensure => present,
         owner => 'root',
         group => 'root',
         mode  => '0644',
       }
-}
+
+
