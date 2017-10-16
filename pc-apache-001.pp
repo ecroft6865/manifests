0f7122b48fb1f293b06b6c38eff6c0b3d634ec43
ecroft6865 <32076639+ecroft6865@users.noreply.github.com>
2017-10-16 08:14:51 -0700

    Delete pc-apache-001.pp

--- pc-apache-001.pp
+++
@@ -1,6 +0,0 @@
-docker::run { 'pc-apache-001':
-    privileged => true,
-    ensure => absent,
-    command => '/bin/sh -c "while true; do echo Hello, world; sleep 1; done"',
-    image   => 'centos/httpd-24-centos7',
-}
