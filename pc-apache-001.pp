dd90213ad995820df41cfbf8592929e3202c1a68
ecroft6865 <eric_h_croft@hotmail.com>
2017-10-25 19:24:23 -0700

    added 'net' to pc-apache-001

--- pc-apache-001.pp
+++ pc-apache-001.pp
@@ -1,9 +1,10 @@
 docker::run { 'pc-apache-001':
-    privileged => true,
-    #ensure => absent,
-    #command => '/bin/sh -c "while true; do echo Hello, world; sleep 1; done"',
-    #command => 'httpd -k start'
-    image   => 'centos/httpd-24-centos7',
+    privileged 	=> true,
+    #ensure	=> absent,
+    #command	=> '/bin/sh -c "while true; do echo Hello, world; sleep 1; done"',
+    #command	=> 'httpd -k start'
+    image	=> 'centos/httpd-24-centos7',
+    net		=> 'pc-network-001',
 }
 
 
