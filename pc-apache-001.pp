a403d84c81c56d3f3034c6aadea3804779c8026e
erictherobot <erictherobot@outlook.com>
2017-10-26 11:35:21 -0700

    added IP

--- pc-apache-001.pp
+++ pc-apache-001.pp
@@ -1,10 +1,12 @@
 docker::run { 'pc-apache-001':
-    privileged 	=> true,
-    #ensure	=> absent,
+    privileged  => true,
+    #ensure     => absent,
     #command	=> '/bin/sh -c "while true; do echo Hello, world; sleep 1; done"',
     #command	=> 'httpd -k start'
-    image	=> 'centos/httpd-24-centos7',
-    net		=> 'pc-network-001',
+    image	    => 'centos/httpd-24-centos7',
+    net		    => 'pc-network-001',
+    ip          =>  '172.24.0.64',
+
 }
 
 
