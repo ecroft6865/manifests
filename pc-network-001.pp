360fcd556cd6db7ee2757126ed8f9e69f71e1253
erictherobot <erictherobot@outlook.com>
2017-10-26 09:30:57 -0700

    MOD: updated subnet

--- pc-network-001.pp
+++ pc-network-001.pp
@@ -1,8 +1,8 @@
 docker_network { 'pc-network-001':
     ensure      => present,
     driver      => 'bridge,
-    subnet      => '172.51.1.0/24',
-    gateway     => '172.51.1.1',
-    ip_range    => '172.51.1.5/32',
+    subnet      => '172.24.0.0/24',
+    gateway     => '172.24.0.1',
+    ip_range    => '172.24.0.5/32',
 
 }
