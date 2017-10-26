f606c8ca394a494926d70dade3bb57be54262401
erictherobot <erictherobot@outlook.com>
2017-10-26 09:38:28 -0700

    fixed

--- pc-network-001.pp
+++ pc-network-001.pp
@@ -1,6 +1,6 @@
 docker_network { 'pc-network-001':
     ensure      => present,
-    driver      => 'bridge,'
+    driver      => 'bridge',
     subnet      => '172.24.0.0/24',
     gateway     => '172.24.0.1',
     ip_range    => '172.24.0.5/32',
